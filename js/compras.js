e62de27f24c10841ea767ca54d5d580765fa5855
rrrrtt <and-s321@hotmail.com>
2017-11-16 20:04:21 -0500

    Se agrego el registrar

--- js/compras.js
+++ js/compras.js
@@ -2,9 +2,13 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('GreetingController', ['$scope', '$http', function ($scope, $http) {
+    if (localStorage.getItem("token") == null) {
+        $window.location.href = './inicio.html';
+    }
     $http({
         method: 'GET',
         url: 'http://localhost:8080/backendPpi/rest/compra'
+
     }).then(function successCallback(response) {
         $scope.posts = response.data
     }, function errorCallback(response) {
