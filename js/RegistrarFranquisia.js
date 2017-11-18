d5caac41db2687b31786a31fd7fae3cfff2c981d
rrrrtt <and-s321@hotmail.com>
2017-11-18 11:28:24 -0500

    Otro commit mas

--- js/RegistrarFranquisia.js
+++ js/RegistrarFranquisia.js
@@ -1,25 +1,18 @@
 (function () {
     var myApp = angular.module('myApp', []);
     myApp.controller("GreetingController", ['$scope', '$http', '$window', function ($scope, $http, $window) {
-        var url = 'http://localhost:8080/backendPpi/rest/persona';
+        var url = 'http://localhost:8080/backendPpi/rest/franquicia';
 
-        $scope.registro = function (id = "", name = "", type = "", number = "", gender = "", telephone = "", email = "", address = "") {
+        $scope.registro = function (idFranquicia = "", nombre = "", acronimo = "", rangoInicial = "", rangoFinal = "") {
             var user = new Object();
-            user.idPersona = id;
-            user.nroDocumento = number;
-            user.nombre = name;
-            user.telefono = telephone;
-            user.direccion = address;
-            user.email = email;
-            user.idTipoDocument = {
-                "idTipoDocumento": 1,
-                "nombreDoc": type
-            };
-            user.idSexo = {
-                "idSexo": 1,
-                "nombreSexo": gender
-            };
+            user.idFranquicia = idFranquicia;
+            user.nombre = nombre;
+            user.acronimo = acronimo;
+            user.rangoInicial = rangoInicial;
+            user.rangoFinal = rangoFinal;       
+
             var data = JSON.stringify(user);
+            Console.log(response.data);
             $http({
                 method: "POST",
                 url: url,
@@ -31,7 +24,6 @@
                 var success = "Exito";
                 $window.alert(success);
                 console.log(response.data);
-                // $window.location.reload();
             }, function errorCallback(response) {
                 var error = "Datos incorrectos";
                 $window.alert(error);
