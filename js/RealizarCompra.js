5f656467aab1e0fab29fe662cccd14fc9aa659c6
rrrrtt <and-s321@hotmail.com>
2017-11-28 06:52:57 -0500

    Algunos cambios en el registro cart

--- js/RealizarCompra.js
+++ js/RealizarCompra.js
@@ -1,25 +1,51 @@
 (function () {
     var myApp = angular.module('myApp', []);
     myApp.controller("GreetingController", ['$scope', '$http', '$window', function ($scope, $http, $window) {
-        var url = 'http://localhost:8080/backendPpi/rest/persona';
+        var url = 'http://localhost:8080/backendPpi/rest/tipotarjeta';
+        var urlFranquicia = 'http://localhost:8080/backendPpi/rest/franquicia';
 
-        $scope.registro = function (id = "", name = "", type = "", number = "", gender = "", telephone = "", email = "", address = "") {
-            var user = new Object();
-            user.idPersona = id;
-            user.nroDocumento = number;
-            user.nombre = name;
-            user.telefono = telephone;
-            user.direccion = address;
-            user.email = email;
-            user.idTipoDocument = {
-                "idTipoDocumento": 1,
-                "nombreDoc": type
+        $http({
+            method: "GET",
+            url: urlFranquicia
+        }).then(function (response) {
+            $scope.franquicias = response.data;
+        }, function errorCallback(response) {
+            var error = "Datos incorrectos";
+            $window.alert(error);
+        });
+
+        $scope.registro = function (nombre = "", acronimo = "", tazaInteres = "", plazoMax = "", cupoMax = "", multa = "", nombreFranquicia = "") {
+
+            var card = new Object();
+            var object = new Object();
+            var i = 0;
+            while(i < $scope.franquicias.length){
+                if($scope.franquicias[i].nombre == nombreFranquicia){
+                    object.idFranquicia = $scope.franquicias[i].idFranquicia;
+                    object.nombre = $scope.franquicias[i].nombre;
+                    object.acronimo = $scope.franquicias[i].acronimo;
+                    object.rangoInicial = $scope.franquicias[i].rangoInicial;
+                    object.rangoFinal = $scope.franquicias[i].rangoFinal;
+                }
+                i++;
+            }
+            
+            card.idTarjeta = "";
+            card.nombreTarjeta = nombre;
+            card.acronimo = acronimo;
+            card.tasaInteres = tazaInteres;
+            card.plazo = plazoMax;
+            card.cupo = cupoMax;
+            card.multa = multa;
+            card.idFranquicia = {
+                "idFranquicia": object.idFranquicia,
+                "nombre": object.nombre,
+                "acronimo": object.acronimo,
+                "rangoInicial": object.rangoInicial,
+                "rangoFinal": object.rangoFinal
             };
-            user.idSexo = {
-                "idSexo": 1,
-                "nombreSexo": gender
-            };
-            var data = JSON.stringify(user);
+            var data = JSON.stringify(card);
+            console.log(data);
             $http({
                 method: "POST",
                 url: url,
@@ -28,14 +54,12 @@
                 },
                 data
             }).then(function (response) {
-                var success = "Exito";
+                var success = "Registrado con Exito";
                 $window.alert(success);
                 console.log(response.data);
-                // $window.location.reload();
             }, function errorCallback(response) {
                 var error = "Datos incorrectos";
                 $window.alert(error);
-                $window.location.reload();
             });
         }
     }]);
--- js/registroTarjeta.js
+++ js/registroTarjeta.js
@@ -2,25 +2,50 @@
     var myApp = angular.module('myApp', []);
     myApp.controller("GreetingController", ['$scope', '$http', '$window', function ($scope, $http, $window) {
         var url = 'http://localhost:8080/backendPpi/rest/tipotarjeta';
+        var urlFranquicia = 'http://localhost:8080/backendPpi/rest/franquicia';
 
-        $scope.registro = function (nombre = "", acronimo = "", tazaInteres = "", plazoMax = "", cupoMax = "", multa = "") {
+        $http({
+            method: "GET",
+            url: urlFranquicia
+        }).then(function (response) {
+            $scope.franquicias = response.data;
+        }, function errorCallback(response) {
+            var error = "Datos incorrectos";
+            $window.alert(error);
+        });
+
+        $scope.registro = function (nombre = "", acronimo = "", tazaInteres = "", plazoMax = "", cupoMax = "", multa = "", nombreFranquicia = "") {
+
             var card = new Object();
-            //  card.codigo = codigo;
-            card.nombre = nombre;
+            var object = new Object();
+            var i = 0;
+            while(i < $scope.franquicias.length){
+                if($scope.franquicias[i].nombre == nombreFranquicia){
+                    object.idFranquicia = $scope.franquicias[i].idFranquicia;
+                    object.nombre = $scope.franquicias[i].nombre;
+                    object.acronimo = $scope.franquicias[i].acronimo;
+                    object.rangoInicial = $scope.franquicias[i].rangoInicial;
+                    object.rangoFinal = $scope.franquicias[i].rangoFinal;
+                }
+                i++;
+            }
+            
+            card.idTarjeta = "";
+            card.nombreTarjeta = nombre;
             card.acronimo = acronimo;
-            card.tazaInteres = tazaInteres;
-            card.plazoMax = plazoMax;
-            card.cupoMax = cupoMax;
+            card.tasaInteres = tazaInteres;
+            card.plazo = plazoMax;
+            card.cupo = cupoMax;
             card.multa = multa;
             card.idFranquicia = {
-                "idFranquicia": 2037,
-                "nombre": "VISAELETRONE",
-                "acronimo": "ELETRONE",
-                "rangoInicial": 3501,
-                "rangoFinal": 3700
-            };            
+                "idFranquicia": object.idFranquicia,
+                "nombre": object.nombre,
+                "acronimo": object.acronimo,
+                "rangoInicial": object.rangoInicial,
+                "rangoFinal": object.rangoFinal
+            };
             var data = JSON.stringify(card);
-
+            console.log(data);
             $http({
                 method: "POST",
                 url: url,
@@ -32,11 +57,9 @@
                 var success = "Registrado con Exito";
                 $window.alert(success);
                 console.log(response.data);
-                $window.location.reload();
             }, function errorCallback(response) {
                 var error = "Datos incorrectos";
                 $window.alert(error);
-                $window.location.reload();
             });
         }
     }]);
