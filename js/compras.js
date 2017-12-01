cb6b6366c82d113f24b3f49aeea0034653e8810e
rrrrtt <and-s321@hotmail.com>
2017-11-30 22:27:03 -0500

    Se Empezo a ordenar las ventanas

--- js/compras.js
+++ js/compras.js
@@ -11,7 +11,7 @@ myApp.controller('GreetingController', ['$scope', '$http', function ($scope, $ht
 
     }).then(function successCallback(response) {
         $scope.posts = response.data
-       // console.log(response.data[0].idTdc.monto);
+       console.log(response.data[0].idTdc.idTarjeta.nombreTarjeta);
     }, function errorCallback(response) {
 
     });
